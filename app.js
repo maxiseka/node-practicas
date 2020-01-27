a96beaf07f643121d8ff8e209c0b670d14530534
Gabriel Casas <gabrielcasas617@gmail.com>
2020-01-27 14:27:44 +0000

    mi primer nodeapp

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+
+const app = (req, res) => {
+  console.info('request to> ', req.url);
+  res.end('gracias x su visita');
+};
+
+
+const server = http.createServer(app);
+
+server.listen(5000, console.info('running on 5000'));
