70d4f2982dfc32bee443cc708358d35a9c5e5735
Camila <camilamatencio@gmail.com>
2020-01-27 16:30:19 -0300

    saved data into mongodb

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const express = require('express');
+const mongodb = require('mongojs');
 
 console.info('iniciando script');
-const app = (req, res) => {
-  console.info('request to> ', req.url);
-  res.end('gracias x su visita');
-};
+const app = express();
+const db = mongodb('mongodb://localhost/tsvcookies');
 
+app.use(express.static('./public'));
+
+app.post('/newcookie', (req, res) => {
+ console.info('new cookie called: ', req.query.value);
+
+ db.cookies.insert({
+     added: new Date(),
+     value: req.query.value,
+ }, (err, doc) => {
+    res.json({
+        ok: err,
+        doc,
+    });
+ });
+ 
+});
 
 const server = http.createServer(app);
 
